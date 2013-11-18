c59bae82054de3905811da39f8513e349261106b
jronigr <jeanne.roniger@gmail.com>
2013-11-18 14:39:27 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlatironAlumniApp::Application.config.secret_key_base = '38acc91ba366a9b48c0e5a831088607bdfc83a0f17b0a7049ad433f0efe01558a0d8bac1a6e7b2442b4b8a0096981ea89fadef6844ab5da2c16443f99a163bc8'
