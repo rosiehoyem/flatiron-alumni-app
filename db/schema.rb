216c0137739a468dab9d1f05d9697bc4fb39d85d
Rosie Hoyem <rosiehoyem@gmail.com>
2013-11-19 18:18:35 -0500

    added devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131119174434) do
+ActiveRecord::Schema.define(version: 20131119223923) do
 
   create_table "students", force: true do |t|
     t.string   "name"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20131119174434) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
 end
