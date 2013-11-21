187a430360bb71b565a9e106a5ef435600b481de
Rosie Hoyem <rosiehoyem@gmail.com>
2013-11-21 11:08:44 -0500

    add forem admin and user role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131120194667) do
+ActiveRecord::Schema.define(version: 20131121155654) do
 
   create_table "forem_categories", force: true do |t|
     t.string   "name",       null: false
@@ -135,6 +135,7 @@ ActiveRecord::Schema.define(version: 20131120194667) do
     t.boolean  "forem_admin",            default: false
     t.string   "forem_state",            default: "pending_review"
     t.boolean  "forem_auto_subscribe",   default: false
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@
 
 # user = User.create(email:'test@gmail.com', password: 'test', password_confirmation:'test')
 # student = user.students.create(firstname:'firstname', lastname:'lastname', city:'New York City', state:'New York', linkedin: 'studlink', twitter: 'studtwit', github: 'studgit', cohort: 'Fall 2013', unique: 'text test hello!')
+
+Forem::Category.create([{:name => 'Events'}, {:name => 'Jobs'}, {:name => 'Resources'}])
+Forem::Group.create([{:name => 'Ruby 003'}, {:name => 'Ruby 002'}, {:name => 'Ruby 001'}])
+# Forem::Topic.create([{:subject => 'ruby topic'}, {:subject => 'Ruby topic 2'}])
+
+# Forem::Category.create([{:name => 'Events'}, {:name => 'Jobs'}, {:name => 'Resources'}])
