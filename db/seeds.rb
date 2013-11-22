b4de5938fc646efcec1c50f37c4b701cceba185e
Rosie Hoyem <rosiehoyem@gmail.com>
2013-11-22 16:00:28 -0500

    edit seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,9 @@
 # user = User.create(email:'test@gmail.com', password: 'test', password_confirmation:'test')
 # student = user.students.create(firstname:'firstname', lastname:'lastname', city:'New York City', state:'New York', linkedin: 'studlink', twitter: 'studtwit', github: 'studgit', cohort: 'Fall 2013', unique: 'text test hello!')
 
-Forem::Category.create([{:name => 'Events'}, {:name => 'Jobs'}, {:name => 'Resources'}])
-Forem::Group.create([{:name => 'Ruby 003'}, {:name => 'Ruby 002'}, {:name => 'Ruby 001'}])
-# Forem::Topic.create([{:subject => 'ruby topic'}, {:subject => 'Ruby topic 2'}])
+Forem::Category.create([{:name => 'Events'}, {:name => 'Jobs'}, {:name => 'Resources'}, {:name => 'Help Line'}])
+Forem::Group.create([{:name => 'Ruby 003'}, {:name => 'Ruby 002'}, {:name => 'Ruby 001'}, {:name => 'Ruby 000'}, {:name => 'iOS 000'}, {:name => 'Fellowship 000'}])
+Forem::Forum.create([{:name => 'Ruby on Rails'}, {:name => 'iOS'}])
 
-# Forem::Category.create([{:name => 'Events'}, {:name => 'Jobs'}, {:name => 'Resources'}])
+
+# Forem::Topic.create([{:subject => 'ruby topic'}, {:subject => 'Ruby topic 2'}])
