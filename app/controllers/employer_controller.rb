2833aa1e9f55854b318949ce765ad5a363367fe2
Edina Vath <Edina.Vath@gmail.com>
2013-11-26 16:52:12 -0500

    employe signup and profile works

--- app/controllers/employer_controller.rb
+++ app/controllers/employer_controller.rb
@@ -9,4 +9,18 @@ class EmployerController < ApplicationController
     @emp = User.find(params[:id])
   end
 
+  def updy
+    emp = User.find(params[:id])
+    emp.update(user_params)
+    redirect_to employer_path
+  end  
+
 end
+
+
+  
+private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :firstname, :lastname, :company)
+  end
