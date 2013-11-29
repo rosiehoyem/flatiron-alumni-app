339a34cbbdea147466cbf867b01398ef5c9351ec
Edina Vath <Edina.Vath@gmail.com>
2013-11-29 09:12:52 -0500

    forum#index styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
 
   def forem_user
     current_user
@@ -16,9 +19,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
 
   private
 
