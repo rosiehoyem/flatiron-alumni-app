077a0e1fbc6d6c7d35cdc3812dcc9c525cc87bf5
tiffanypeon <tiffanypeon@gmail.com>
2013-12-01 14:05:41 -0500

    added edit profile link to dashboard

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -36,6 +36,10 @@ class AttachmentUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [200, 200]
   end
 
+  version :teensy do 
+    process :resize_to_fill => [50, 50]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
