6a506c3f2693f223e682acb0afc2129e5a5438e6
Rosie Hoyem <rosiehoyem@gmail.com>
2013-12-10 13:25:44 -0500

    cleaned up files and edited readme

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -12,5 +12,6 @@ FactoryGirl.define do
     twitter "twitter"
     github "github"
     cohort "cohort"
+    photo Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/fixtures/myfiles/myfile.jpg')))
   end
 end
